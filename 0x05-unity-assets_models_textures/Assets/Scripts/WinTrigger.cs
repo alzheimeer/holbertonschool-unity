71c765f65071fc514f19cccbe8140cfd4317ac8a
Edgar <fogniebla@hotmail.com>
2020-06-10 20:42:52 -0500

    fix

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+    public Text timerText;
+    public GameObject playerT;
+
+    void OnTriggerEnter(Collider other)
+    {
+        playerT.GetComponent<Timer>().enabled = false;
+        timerText.fontSize = 60;
+        timerText.color = Color.green;
+    }
+}
