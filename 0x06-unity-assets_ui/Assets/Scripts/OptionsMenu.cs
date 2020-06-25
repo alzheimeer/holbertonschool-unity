74d5ab2f1e34bb91e2e56a143e18d928a0244410
Edgar <fogniebla@hotmail.com>
2020-06-24 19:17:29 -0500

    feature task 3

---
+++ 0x06-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public void Back()
+    {
+        string sceneName = PlayerPrefs.GetString("lastScene");
+        // PlayerPrefs.SetString("lastScene", SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(sceneName);
+    }
+}
