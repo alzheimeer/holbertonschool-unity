3332a56d46e95bb077c882088c567ea721aa9009
Edgar <fogniebla@hotmail.com>
2020-07-07 22:18:02 -0500

    feature task 1

---
+++ 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera camtwo;
+    void Start()
+    {
+        camtwo = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("q")) camtwo.enabled = false;
+        if (Input.GetKeyDown("z")) camtwo.enabled = true;
+
+    }
+}
