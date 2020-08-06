d71623b5a458205485fc560b5543b38b63153c58
Edgar <fogniebla@hotmail.com>
2020-08-05 22:58:46 -0500

    fix

--- 0x09-unity_ar_business_card/Assets/Scripts/botons.cs
+++ 0x09-unity_ar_business_card/Assets/Scripts/botons.cs
@@ -12,18 +12,6 @@ public class botons : MonoBehaviour
     public Transform face;
     public Transform inst;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
 
     public void botonP()
     {
